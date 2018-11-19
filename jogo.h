7f041e8f505b8a242d87f0700d83d841de7a33dd
Matheus Schimieguel Silva <40153186+mschimieguel@users.noreply.github.com>
2018-11-19 19:01:26 -0200

    Update jogo.h

--- jogo.h
+++ jogo.h
@@ -29,19 +29,10 @@
 			void print_Jogo (); 
 			int rodada();
 			void efeitos_de_carta(Carta *atual);
-
-			// int cnt_compra_2();
-			// int cnt_compra_4();
 			int cnt_de_carta(char valor);
-			
 			void passa_rodada();
 			void repoe_baralho();
-
 			void inicia_jogo();
-		
-			
-
-
 	};
 
 
