6d600bdb34d3309aeb3d79a30e99ae31800aa140
TiagoTozo <tozotiago@gmail.com>
2018-11-22 18:34:37 -0200

    IA É SO IF!!!

--- bot.h
+++ bot.h
@@ -1,16 +1,18 @@
 #ifndef BOT_H
-#define BOT_H 
+#define BOT_H
 #include "jogador.h"
-	
+
 
 
 	class Bot : public Jogador {
 		public:
-			Bot(std::string nome);
-			~Bot();
-			virtual Carta* jogada (Carta *atual) override;
-			virtual Carta* rebate(char valor) override;
-			
+			Bot(int numero);
+			//~Bot();
+			//virtual Carta* jogada (Carta *atual) override;
+			//virtual Carta* rebate(char valor) override;
+			virtual void escolhe_cor(Carta *escolhida) override;
+			virtual bool vai_jogar() override;
+			//char melhor_cor();
 	};
 
 
