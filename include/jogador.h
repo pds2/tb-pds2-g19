bdae3e3a109c64b714ca80c3466846a1794f44a5
TiagoTozo <tozotiago@gmail.com>
2018-11-29 16:30:28 -0200

    poder escolher ver a mao do bot

--- include/jogador.h
+++ include/jogador.h
@@ -12,8 +12,9 @@
 		protected:
 			std::string _nome;
 			std::vector<Carta*> _mao;
-			
+
 		public:
+		  	static bool print_bots;
 			//CONTRUTOR E DESTRUTOR
 			Jogador();
 			Jogador(std::string nome);
@@ -25,8 +26,8 @@
 			int num_cartas() const;
 			void compra_carta(Baralho &baralho,int n = 1);
 			virtual Carta* jogada (Carta *atual);
-			void print_mao() const;
-			void print_mao(char valor) const;
+			virtual void print_mao() const;
+			virtual void print_mao(char valor) const;
 			int cartas_jogaveis(Carta *atual) const;
 			int qtd_de_carta(char valor) const;
 			virtual Carta* rebate(char valor);
