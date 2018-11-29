579b1362eba5e114829a1c4b3b62eb44ad8c0bb6
TiagoTozo <tozotiago@gmail.com>
2018-11-29 17:02:22 -0200

    escolhe a mao do bot certo

--- src/bot.cpp
+++ src/bot.cpp
@@ -1,82 +1,193 @@
 #include <iostream>
-#include <limits.h>
+#include <chrono>
+#include <thread>
 #include "jogador.h"
 #include "carta.h"
 #include "jogo.h"
 #include "baralho.h"
+#include "bot.h"
+
+	int Bot::_n_bots = 0;
+
 
 	//CONSTRUTOR E DESTRUTOR
-	bool Jogador::print_bots = false;
 
-	Jogador::Jogador(){}
-
-	Jogador::Jogador(std::string nome){
-		_nome = nome;
+	Bot::Bot(Jogo *jogo):Jogador("Bot0" + std::to_string(1 + _n_bots++)){
+		_jogo = jogo;
 	}
 
-	Jogador::~Jogador(){
-		while(!_mao.empty()){
-			delete _mao[_mao.size() - 1];
-			_mao.pop_back();
-		}
-	}
+	Bot::~Bot(){}
 
 	//METODOS
 
-	std::string Jogador::get_nome() const{
-		return _nome;
-	}
+	Carta* Bot::jogada (Carta *atual){
+		std::this_thread::sleep_for(std::chrono::seconds(2));
+		int sentido = this->_jogo->get_sentido();
+		int jogador_atual = this->_jogo->get_jogador_atual();
+		int n_participantes =  this->_n_bots + this->_jogo->get_n_jogadores();
 
-	void Jogador::set_nome(std::string nome){
-		_nome = nome;
-	}
+		int pos_proximo_jogador = (jogador_atual + sentido)% n_participantes;
 
-	int Jogador::num_cartas() const{
-		return _mao.size();
-	}
 
-	void Jogador::compra_carta(Baralho &baralho,int n){
-		if(baralho.get_tamanho() >= n ){
-			for (int i = 0 ; i < n;i++ ){
-				_mao.push_back(baralho.get_ultima_carta());
-				baralho.remove_fim();
+		if(pos_proximo_jogador  < 0)
+			pos_proximo_jogador += n_participantes;
+		Jogador *proximo_jogador = this->_jogo->get_jogador(pos_proximo_jogador);
+
+		Carta *to_return;
+		to_return = nullptr;
+
+		if (proximo_jogador->num_cartas() <= 3){
+			to_return = this->atrapalha_proximo(atual);
+			if(to_return != nullptr ){
+				std::cout << "O " << this->get_nome() << " jogou : ";
+				to_return->print_carta();
+				std::this_thread::sleep_for(std::chrono::seconds(2));
+				return to_return;
 			}
 		}
-		else
-			std::cout<<"Acabaram as cartas do baralho e nao foi possivel comprar " <<std::endl;
-	}
-
-	Carta* Jogador::jogada (Carta *atual){
-		//retorna  o ponteiro da carta jogada pelo jogador
-		std::cout<<"Escolha a posicao da carta de sua mao para jogar : " << std::endl;
-		 int pos;
-		std::cin >> pos;
-
-		while( std::cin.fail() || pos < 0 || pos > this->num_cartas()-1  ){
-			if( std::cin.fail()){
-				std::cin.clear();
-				std::cin.ignore(INT_MAX,'\n');
-				std::cout<< "Jogada invalida "<<std::endl;
+		//dando preferencia a cartas numeros
+		for(int i = 0;i < this->num_cartas();i++){
+			if( (_mao[i]->get_valor() == atual->get_valor()) && _mao[i]->get_valor() != COMPRA_2 && _mao[i]->get_valor() != CORINGA && _mao[i]->get_valor() != COMPRA_4){
+				to_return = _mao[i];
+				_mao.erase(_mao.begin() + i);
+				std::cout << "O " << this->get_nome() << " jogou : ";
+				to_return->print_carta();
+				std::this_thread::sleep_for(std::chrono::seconds(2));
+				return to_return;
 			}
-			std::cout<<"A posicao escolhida nao existe, favor escolher entre 0 e " << this->num_cartas()-1 << std::endl;
-			std::cin >> pos;
 		}
 
-		Carta *to_return = _mao[pos];
-
-		if (to_return->get_cor() == ESPECIAL || to_return->get_cor() == atual->get_cor() || to_return->get_valor() == atual->get_valor()){
-			_mao.erase(_mao.begin() + pos);
-			std::cin.clear();
-			return to_return;
+		//dando preferencia a cartas normais
+		for(int i = 0;i < this->num_cartas();i++){
+			if( (_mao[i]->get_cor() == atual->get_cor() || _mao[i]->get_valor() == atual->get_valor()) && _mao[i]->get_valor() != COMPRA_2 && _mao[i]->get_valor() != CORINGA && _mao[i]->get_valor() != COMPRA_4){
+				to_return = _mao[i];
+				_mao.erase(_mao.begin() + i);
+				std::cout << "O " << this->get_nome() << " jogou : ";
+				to_return->print_carta();
+				std::this_thread::sleep_for(std::chrono::seconds(2));
+				return to_return;
+			}
 		}
-		else{
-			std::cout<<"A carta esolhida nao pode ser jogada" << std::endl;
-			return this->jogada(atual);
+		for(int i=0;i<this->num_cartas();i++){
+			if(_mao[i]->get_cor() == atual->get_cor() || _mao[i]->get_valor() == atual->get_valor() ||_mao[i]->get_cor()==ESPECIAL ){
+				to_return = _mao[i];
+				_mao.erase(_mao.begin() + i);
+				std::cout << "O " << this->get_nome() << " jogou : ";
+				to_return->print_carta();
+				std::this_thread::sleep_for(std::chrono::seconds(2));
+				return to_return;
+			}
 		}
+		return nullptr;
 	}
 
-	void Jogador::print_mao() const	{
-		int tam = this->num_cartas();
+	Carta* Bot::rebate(char valor){
+		std::this_thread::sleep_for(std::chrono::seconds(3));
+		Carta *to_return;
+		for(int i=0;i<this->num_cartas();i++){
+			if(_mao[i]->get_valor() == valor ){
+				to_return = _mao[i];
+				_mao.erase(_mao.begin() + i);
+				std::cout << "O " << this->get_nome() << " jogou : ";
+				to_return->print_carta();
+				std::this_thread::sleep_for(std::chrono::seconds(3));
+				return to_return;
+			}
+		}
+		return nullptr;
+	}
+
+	void Bot::escolhe_cor(Carta *escolhida){
+		char cor;
+
+		int n_red = qtd_de_carta(RED);
+		int n_blue = qtd_de_carta(BLUE);
+		int n_green = qtd_de_carta(GREEN);
+		int n_yellow = qtd_de_carta(YELLOW);
+
+		if(n_red>=n_blue&&n_red>=n_green&&n_red>=n_yellow)cor = RED;
+		if(n_blue>=n_red&&n_blue>=n_green&&n_blue>=n_yellow)cor = BLUE;
+		if(n_green>=n_blue&&n_green>=n_red&&n_green>=n_yellow)cor = GREEN;
+		if(n_yellow>=n_blue&&n_yellow>=n_green&&n_yellow>=n_red)cor = YELLOW;
+
+		Carta collor(cor,'0');
+		std::cout << "O " << this->get_nome() << " escolheu : ";
+		collor.print_carta_cor();
+		std::cout <<std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+
+		escolhida->set_cor(cor);
+	}
+
+	bool Bot::vai_jogar(){return true;}
+
+	Carta *Bot::atrapalha_proximo(Carta *atual){
+
+		Carta *to_return[7] = {nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,nullptr};
+
+		if (this->qtd_de_carta(COMPRA_2) > 0)
+			if (atual->get_valor() == COMPRA_2)
+				to_return[0] = this->escolhe_carta(COMPRA_2);
+
+		if (this->qtd_de_carta(COMPRA_4 ) > 0)
+			if (atual->get_valor() == COMPRA_4)
+				to_return[2] =this->escolhe_carta(COMPRA_4);
+
+		if (this->qtd_de_carta(PULAR)>  0)
+			if (atual->get_valor() == PULAR)
+				to_return[3] =	this->escolhe_carta(PULAR);
+
+		if (this->qtd_de_carta(REVERTER) > 0 )
+			if (atual->get_valor() == REVERTER )
+				to_return[5] =  this->escolhe_carta(REVERTER);
+
+		if (this->qtd_de_carta(COMPRA_2) > 0)to_return[1] = this->escolhe_carta(COMPRA_2,atual->get_cor());
+		if (this->qtd_de_carta(PULAR)> 0)to_return[4] = this->escolhe_carta(PULAR,atual->get_cor());
+		if (this->qtd_de_carta(REVERTER) > 0)to_return[6] = this->escolhe_carta(REVERTER,atual->get_cor());
+
+		for (int i = 0; i < 7;i++){
+			if (to_return[i] != nullptr){
+				this->tira_carta_mao(to_return[i]);
+				return to_return[i];
+			}
+		}
+		return nullptr;
+	}
+
+	Carta *Bot::escolhe_carta(char valor){
+		Carta *to_return;
+		for(int i=0;i<this->num_cartas();i++){
+			if( _mao[i]->get_valor() == valor ){
+				to_return = _mao[i];
+				return to_return;
+			}
+		}
+		return nullptr;
+	}
+
+	Carta *Bot::escolhe_carta(char valor,char cor){
+		Carta *to_return;
+		for(int i=0;i<this->num_cartas();i++){
+			if((_mao[i]->get_valor() == valor  && _mao[i]->get_cor() == cor) || (_mao[i]->get_valor() == valor && valor == CORINGA)  || (_mao[i]->get_valor() == valor && valor == COMPRA_4) ){
+				to_return = _mao[i];
+				return to_return;
+			}
+		}
+		return nullptr;
+	}
+
+	void  Bot::tira_carta_mao(Carta *escolhida){
+		for (int i = 0;i < this->num_cartas() ; i++)
+			if (_mao[i] == escolhida){
+				_mao.erase(_mao.begin() + i);
+				break;
+			}
+	}
+  void Bot::print_mao()const{
+    if(!print_bots)
+       std::cout<<"O bot: "<<this->get_nome()<<" tem "<<this->num_cartas()<<" cartas\n"<<std::endl;
+    else{
+      int tam = this->num_cartas();
 		std::cout << _nome << " : "<< std::endl;
 		std::cout<<std::endl;
 		for (int i = 0 ; i < tam ; i++){
@@ -84,105 +195,20 @@
 			_mao[i]->print_carta();
 		}
 		std::cout <<std::endl;
-	}
-
-	void Jogador::print_mao(char valor) const	{
-		int tam = this->num_cartas();
-		std::cout << _nome << " : "<< std::endl;
-		std::cout<<std::endl;
-		for (int i = 0 ; i < tam ; i++){
-			if(_mao[i]->get_valor() == valor  ){
-				std::cout<< "  " << i << " ";
-				_mao[i]->print_carta();
-			}
-		}
+    }
+  }
+  void Bot::print_mao(char valor)const{
+    if(!print_bots)return;
+    else{
+      int tam = this->num_cartas();
+		  std::cout << _nome << " : "<< std::endl;
+		  std::cout<<std::endl;
+		  for (int i = 0 ; i < tam ; i++){
+			  if(_mao[i]->get_valor() == valor  ){
+				  std::cout<< "  " << i << " ";
+				  _mao[i]->print_carta();
+			  }
+		  }
 		std::cout <<std::endl;
-	}
-
-	int Jogador::cartas_jogaveis(Carta *atual) const{
-		//retorna a quantidade de cartas jogaveis pelo jogador tendo em base a carta jogada pelo ultimo jogador
-		int n_cartas_jogaveis = 0;
-		for (int i = 0;i < this->num_cartas();i++){
-			if (_mao[i]->get_cor() == ESPECIAL || _mao[i]->get_cor() == atual->get_cor() || _mao[i]->get_valor() == atual->get_valor() ){
-				n_cartas_jogaveis++;
-			}
-		}
-		return n_cartas_jogaveis;
-	}
-
-	int Jogador::qtd_de_carta(char valor) const{
-		int  cnt= 0;
-		for (int i = 0;i < this->num_cartas();i++){
-			if ( _mao[i]->get_valor() == valor   ){
-				cnt++;
-			}
-		}
-		return cnt;
-	}
-
-	Carta* Jogador::rebate(char valor){
-		Carta value('r',valor);
-		int pos = 0;
-
-		std::cout<<"Escolha a posicao da carta ";
-		value.print_carta_valor();
-		std::cout <<" de sua mao para jogar :" << std::endl;
-		std::cin >> pos;
-
-		while( std::cin.fail() || pos < 0 || pos > this->num_cartas()-1 || _mao[pos]->get_valor() != valor){
-			if( std::cin.fail()){
-				std::cin.clear();
-				std::cin.ignore(INT_MAX,'\n');
-				std::cout<< "Jogada invalida, carta inexistente. Jogue novamente"<<std::endl;
-			}
-			if (_mao[pos]->get_valor() != valor){
-				std::cout<<"Erro :a carta escolhida nao e uma ";
-				value.print_carta_valor();
-				std::cout <<" .Jogue novamente" << std::endl;
-			}
-			std::cin >> pos;
-		}
-		Carta *to_return = _mao[pos];
-		_mao.erase(_mao.begin() + pos);
-		return to_return;
-	}
-
-	void Jogador::escolhe_cor(Carta *escolhida){
-		char cor;
-		if (escolhida->get_cor() == ESPECIAL){
-			std::cout << " Escolha a cor que deseja entre (r,b,g,y) : " << std::endl;
-			std::cin >> cor;
-
-			while(std::cin.fail() || (cor != 'r' && cor != 'b' && cor != 'g' && cor != 'y')){
-				if( std::cin.fail()){
-					std::cin.clear();
-					std::cin.ignore(INT_MAX,'\n');
-				}
-			  	std::cout << " Cor invalida escolhida, escolha entre (r,b,g,y) : " << std::endl;
-				std::cin >> cor;
-			}
-			escolhida->set_cor(cor);
-		}
-		else{
-			std::cout << "Programador : Tal carta nao pode mudar de cor durante a partida"<<std::endl;
-		}
-	}
-
-	bool Jogador::vai_jogar(){
-		char jogador_n_quer_jogar;
-		std::cout << "Vc quer jogar('j') ou pular('p')?\n";
-		std::cin >> jogador_n_quer_jogar;
-		while(std::cin.fail()||(jogador_n_quer_jogar!='j'&&jogador_n_quer_jogar!='p')){
-			if(std::cin.fail()){
-				std::cin.clear();
-				std::cin.ignore(INT_MAX,'\n');
-			}
-			std::cout << "Vc quer jogar('j') ou pular('p')?\n";
-			std::cin >> jogador_n_quer_jogar;
-		}
-		if(jogador_n_quer_jogar=='p')return false;
-		else if(jogador_n_quer_jogar=='j')return true;
-		else return false;
-	}
-
-
+    }
+  }
