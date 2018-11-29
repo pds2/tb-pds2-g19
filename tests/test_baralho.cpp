c153490c68d7c6a729a66cab625408f33eaa1550
helenapato <42720360+helenapato@users.noreply.github.com>
2018-11-29 15:30:15 -0200

    Testes Baralho

--- tests/test_baralho.cpp
+++ tests/test_baralho.cpp
@@ -1,5 +1,26 @@
 #include "doctest.h"
 #include "carta.h"
 #include "baralho.h"
-#include "jogador.h"
-#include "bot.h"
+
+#define TOTAL_CARTAS 108
+
+    TEST_CASE("Testando Construtor"){
+        Baralho a;
+
+        CHECK(a.get_tamanho() == TOTAL_CARTAS);
+    }
+
+    TEST_CASE("Testando Funções remove_fim, adiciona_carta e get_ultima_carta"){
+        Baralho a;
+        CHECK(a.get_tamanho() == TOTAL_CARTAS);
+
+        a.remove_fim();
+        CHECK(a.get_tamanho() == TOTAL_CARTAS-1);
+
+        Carta *b = new Carta(RED, '0');
+
+        a.adiciona_carta(b);
+        CHECK(a.get_tamanho() == TOTAL_CARTAS);
+
+        CHECK(a.get_ultima_carta() == b);
+    }
